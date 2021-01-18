7626962df168ffd1d2f333bbea507456bbc1c5f6
Hector Avalos <gerardo.avalos97@hotmail.com>
2021-01-18 11:57:48 -0600

    Updated MongooseModule connection string. Replaced .forRoot() with .forRootAsync(), allowing ConfigModule and ConfigService to get the .env values.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ProductModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-api', {
-      useFindAndModify: false,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const HOST = configService.get<string>('MONGO_HOST');
+        const PORT = configService.get<string>('MONGO_PORT');
+        const DB_NAME = configService.get<string>('DB_NAME');
+
+        return {
+          uri: `mongodb://${HOST}:${PORT}/${DB_NAME}`,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
